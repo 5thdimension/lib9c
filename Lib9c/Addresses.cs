4380b982df0013d6f0e390fd58ffc0758e6513df
s2quake <han0210@netsgo.com>
2024-10-25 21:37:22 +0900

    feat: Add tx actions for collecting used-gas

--- Lib9c/Addresses.cs
+++ Lib9c/Addresses.cs
@@ -48,6 +48,8 @@ namespace Nekoyume
         public static readonly Address DailyReward           = new("0000000000000000000000000000000000000020");
         public static readonly Address ActionPoint           = new("0000000000000000000000000000000000000021");
         public static readonly Address RuneState             = new("0000000000000000000000000000000000000022");
+        public static readonly Address MortgagePool          = new("0000000000000000000000000000000000100000");
+        public static readonly Address GasPool               = new("0000000000000000000000000000000000100001");
 
         // Custom Equipment Craft
         public static readonly Address Relationship          = new("0000000000000000000000000000000000000023");
